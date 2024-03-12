7287c4ca0ceb7c6589638639803fd6c58721823a
Umar Farooq <umarfarooq567800@gmail.com>
2024-03-12 12:51:03 +0500

    Load more button done

--- 6-load-more-btn/Steps.md
+++ 6-load-more-btn/Steps.md
@@ -5,17 +5,51 @@ When page load, it displays some dummy products, at last of the page there is a 
 - For this functionality we use a dummyjson api endpint
 - this api provides some functioalities, we can set limit of products, number of prooducts we want to get, skip, and select based on desired scenerio
 
-
 ### set up project
-
-
 
 ### Working on **LoadMoreBtn.jsx**
 
-- declear state variables for 
+- declear state variables for
+
   - loading
   - products array
   - count
 
-
 - Make an async `fetchProducts` method to call api
+- api url: `https://dummyjson.com/products?limt=10&skip=10`
+- we can set limt and also skip items
+- when we first call the api, we recive 10 products, and we set the state of products as an array of 10 products
+- we set the state of count as 1
+- next time , we will skip 10 products and get 10 more products, and we will add them to the products array
+- how we do: writing skip logic in the url: if the count is 0, the skipping is 0, otherwise it is 10 \* count
+- multipling the count by 10 will give us the number of products we want to skip
+
+```js
+`https://dummyjson.com/products?limt=10&skip=${count === 0 ? 0 : count * 10}`;
+```
+
+- make a const result to store response from the api
+
+- - run fetchProduct when page loads using useEffect
+
+then
+
+- check is result, results.products, and result.products.lenght is true , then set the state of products as result.products
+
+- set loading true before calling api
+- set loading false after data is reicved, or if there is any error
+
+- Write Jsx to display products, write rendring logic etc
+- make load more button , and set onClick `onClick={() => setCount(count + 1)}` incrememt count by one
+- then update the setProducts, destructure the products array from the state and add more data along with prevoius data
+  `setProducts((prev) => [...prev, ...result.products]);`
+
+  ### Problem
+
+  - Use Strict causes rendering two time of component
+  - first it mount the component , and set the state of products array
+  - when it renders 2nd time it over write the state of products array
+  - that was causing problem, i was reciving 30 products instead of 10 products
+
+  - so , i disabled strict mode
+  - problem resolved!
--- 6-load-more-btn/src/components/LoadMoreBtn.jsx
+++ 6-load-more-btn/src/components/LoadMoreBtn.jsx
@@ -1,9 +1,82 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const LoadMoreBtn = () => {
   const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
 
-  return <div>LoadMoreBtn</div>;
+  async function fetchProducts() {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=10&skip=${count * 10}`
+      );
+
+      const result = await response.json();
+      if (result && result.products && result.products.length > 0) {
+        setProducts((prev) => [...prev, ...result.products]);
+      }
+
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [count]);
+
+  if (loading) {
+    return (
+      <h3 className="text-emerald-500 font-bold text-center text-2xl my-10 ">
+        Loading...
+      </h3>
+    );
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="font-bold text-2xl text-emerald-400 text-center mt-10">
+        Dummy Json Products || click load more button to see more products
+      </h1>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-wrap justify-between mt-10">
+        {products && products.length
+          ? products.map((item) => (
+              <div
+                key={item.id}
+                className="flex items-center justify-between p-2 border-b border-gray-200 w-80"
+              >
+                <div className="flex items-center ">
+                  <img src={item.images[2]} className="w-40 h-40 rounded-lg" />
+                  <div className="ml-4 ">
+                    <div className="text-sm font-medium text-gray-100">
+                      {item.title}
+                    </div>
+                    <div className="text-sm text-gray-300 ">
+                      {item.category}
+                    </div>
+                    <div className="text-sm text-yellow-500 mt-2">
+                      {item.price}$
+                    </div>
+                    <h3 className="text-emerald-500 font-semibold">
+                      Product No. {item.id}
+                    </h3>
+                  </div>
+                </div>
+              </div>
+            ))
+          : null}
+      </div>
+      <button
+        className="px-4 py-3 bg-emerald-500 hover:bg-white hover:text-emerald-500 transition-colors w-max  rounded-lg text-white font-bold mb-10 "
+        onClick={() => setCount(count + 1)}
+      >
+        Load More
+      </button>
+    </div>
+  );
 };
 
 export default LoadMoreBtn;
